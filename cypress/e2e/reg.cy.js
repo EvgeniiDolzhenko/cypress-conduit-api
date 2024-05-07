242cc99f5585a5353fea65da4065ad63ab8ae528
Eugene <zhrobot@gmail.com>
2024-05-07 10:28:28 -0400

    reg helper

--- cypress/e2e/reg.cy.js
+++ cypress/e2e/reg.cy.js
@@ -1,5 +1,5 @@
 import {  faker } from '@faker-js/faker';
-
+import { registration } from '../pages/reg';
 const api_server = Cypress.env('api_server')
 
 describe('Register new client',()=>{
@@ -8,17 +8,12 @@ describe('Register new client',()=>{
     const username = faker.person.fullName()
 
     it('positive new user',()=>{
-        cy.api({
-            method:'POST',
-            url: `${api_server}/users`,
-            body: {
-                "user": {
-                  "email": email,
-                  "password": "123",
-                  "username": username
-                }
-              }
-        }).then((response)=>{
+        const data = {
+            "email": email,
+            "password": "123",
+            "username": username
+        }
+        registration.registerNewClient(api_server,data).then((response)=>{
             console.log(response.body.user.id);
             expect(response.body.user.id).a('number')
             expect(response.status).eq(201)
@@ -26,61 +21,45 @@ describe('Register new client',()=>{
             expect(response.body.user.username).eq(username)
             expect(response.body.user.token).a('string')
         })
-
     })
 })
 
 describe('Register new client negative',()=>{
     const email = faker.internet.email()
     const username = faker.person.fullName()
-
     it('Register new client without email',()=>{
-        cy.api({
-            method:'POST',
-            url: `${api_server}/users`,
-            failOnStatusCode:false,
-            body: {
-                "user": {
-                  "password": "123",
-                  "username": username
-                }
-              }
-        }).then((response)=>{
-            expect(response.status).eq(422)
-            expect(response.body.errors.email).deep.eq(["can't be blank"])
+        const data = {
+            "password": "123",
+            "username": username
+        }
+        registration.registerNewClient(api_server,data)
+            .then((response)=>{
+                expect(response.status).eq(422)
+                expect(response.body.errors.email).deep.eq(["can't be blank"])
         })
     })
 
     it('Register new client without username',()=>{
-        cy.api({
-            method:'POST',
-            url: `${api_server}/users`,
-            failOnStatusCode:false,
-            body: {
-                "user": {
-                    "email": email,
-                    "password": "123",
-                }
-              }
-        }).then((response)=>{
-            expect(response.status).eq(422)
-            expect(response.body.errors.username).deep.eq(["can't be blank"])
+        const data =  {
+            "email": email,
+            "password": "123",
+        }
+
+        registration.registerNewClient(api_server,data)
+            .then((response)=>{
+                expect(response.status).eq(422)
+                expect(response.body.errors.username).deep.eq(["can't be blank"])
         })
     })
 
     it('Register new client without username and email',()=>{
-        cy.api({
-            method:'POST',
-            url: `${api_server}/users`,
-            failOnStatusCode:false,
-            body: {
-                "user": {
-                    "password": "123",
-                }
-              }
-        }).then((response)=>{
-            expect(response.status).eq(422)
-            expect(response.body.errors.email).deep.eq(["can't be blank"])
+        const data = {
+            "password": "123",
+        }
+        registration.registerNewClient(api_server,data)
+            .then((response)=>{
+                expect(response.status).eq(422)
+                expect(response.body.errors.email).deep.eq(["can't be blank"])
         })
     })
 
@@ -88,69 +67,50 @@ describe('Register new client negative',()=>{
         const newEmail = faker.internet.email()
         const newUsername = faker.person.fullName()
         const random = Math.floor(100000 + Math.random() * 900000)
-        cy.api({
-            method:'POST',
-            url: `${api_server}/users`,
-            body: {
-                "user": {
-                  "email": newEmail,
-                  "password": "123",
-                  "username": newUsername
-                }
-              }
-        }).then((response)=>{
-            expect(response.status).eq(201)
-        }).then(()=>{
-            cy.api({
-                method:'POST',
-                url: `${api_server}/users`,
-                failOnStatusCode:false,
-                body: {
-                    "user": {
-                      "email": newEmail,
-                      "password": "123",
-                      "username": newUsername+random
-                    }
-                  }
-            }).then((response)=>{
-                expect(response.status).eq(422)
-                expect(response.body.errors.email["has already been taken"])
-
+        const data = {
+            "email": newEmail,
+            "password": "123",
+            "username": newUsername
+        }
+        const dataRandomName = {
+            "email": newEmail,
+            "password": "123",
+            "username": newUsername+random
+        }
+        registration.registerNewClient(api_server,data)
+            .then((response)=>{
+                expect(response.status).eq(201)
+                }).then(()=>{
+                    registration.registerNewClient(api_server,dataRandomName)
+                    .then((response)=>{
+                        expect(response.status).eq(422)
+                        expect(response.body.errors.email["has already been taken"])
             })
         })
     })
 
-    it('Register client with existiin email',()=>{
+    it('Register client with existiin username',()=>{
         const newEmail = faker.internet.email()
         const newUsername = faker.person.fullName()
         const random = Math.floor(100000 + Math.random() * 900000)
-        cy.api({
-            method:'POST',
-            url: `${api_server}/users`,
-            body: {
-                "user": {
-                  "email": newEmail,
-                  "password": "123",
-                  "username": newUsername
-                }
-              }
-        }).then((response)=>{
-            expect(response.status).eq(201)
-        }).then(()=>{
-            cy.api({
-                method:'POST',
-                url: `${api_server}/users`,
-                failOnStatusCode:false,
-                body: {
-                    "user": {
-                      "email": newEmail+random,
-                      "password": "123",
-                      "username": newUsername
-                    }
-                  }
-            }).then((response)=>{
-                expect(response.status).eq(422)
-                expect(response.body.errors.username["has already been taken"])
+        const data =  {
+            "email": newEmail,
+            "password": "123",
+            "username": newUsername
+        }
+        const dataRandomMail =  {
+            "email": newEmail+random,
+            "password": "123",
+            "username": newUsername
+        }
+        registration.registerNewClient(api_server,data)
+            .then((response)=>{
+                expect(response.status).eq(201)
+                }).then(()=>{
+                    registration.registerNewClient(api_server,dataRandomMail)
+                    .then((response)=>{
+                        expect(response.status).eq(422)
+                        expect(response.body.errors.username["has already been taken"])
 
             })
         })
---
+++ cypress/pages/reg.js
@@ -0,0 +1,16 @@
+export class Registration{
+
+    registerNewClient(url,data){
+        return  cy.api({
+            method:'POST',
+            url: `${url}/users`,
+            failOnStatusCode:false,
+            body: {
+                "user": data
+              }
+        })
+    }
+
+
+}
+export const registration = new Registration
