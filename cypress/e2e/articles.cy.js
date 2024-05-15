594af587615eec3f86d65403f4930a7cb072d952
Eugene <zhrobot@gmail.com>
2024-05-15 16:25:23 -0400

    create new article

--- cypress/e2e/articles.cy.js
+++ cypress/e2e/articles.cy.js
@@ -1,8 +1,8 @@
 const email = Cypress.env('email')
 const pass = Cypress.env('pass')
 const api_server = Cypress.env('api_server')
-
-describe('Articles positive',()=>{
+import { faker} from '@faker-js/faker';
+describe('Cet all articles',()=>{
     it('verify list of the articles',()=>{
             cy.api({
                 method:'GET',
@@ -13,5 +13,35 @@ describe('Articles positive',()=>{
             }).then((response)=>{
                 expect(response.status).eq(200)
             })
+    })
+})
+
+describe('Create new article, verify , edit, delete E2E API',()=>{
+
+    const title = faker.lorem.words(1);
+    const description = faker.lorem.sentences(1)
+    const articleInfo = faker.lorem.sentences(3)
+    it('Create new article',()=>{
+        cy.api({
+            method: "POST",
+            url: `${api_server}/articles`,
+            headers:{
+                Authorization: 'Token '+Cypress.env('token')
+            },
+            body:{
+                "article": {
+                    "title": title,
+                    "description": description,
+                    "body": articleInfo,
+                    "tagList": [
+                        'fashion', 'art', 'music'
+                    ]
+                }
+            }
+        }).then((response)=>{
+            expect(response.status).eq(201)
+            expect(response.body.article.slug).include(title+'-2980')
+        })
+
     })
 })
