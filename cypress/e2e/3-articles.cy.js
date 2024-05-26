b390ab7a886cce9234ff84279576525565cfd9d4
Eugene <zhrobot@gmail.com>
2024-05-26 13:43:08 -0400

    create new article

--- cypress/e2e/3-articles.cy.js
+++ cypress/e2e/3-articles.cy.js
@@ -1,6 +1,8 @@
 const email = Cypress.env('email')
 const pass = Cypress.env('pass')
 const api_server = Cypress.env('api_server')
+
+import { articlePage } from '../pages/articles'
 import { faker} from '@faker-js/faker';
 describe('Cet all articles',()=>{
     it('verify list of the articles',()=>{
@@ -17,45 +19,30 @@ describe('Cet all articles',()=>{
 })
 
 describe('Create new article, verify , edit, delete E2E API',()=>{
-
+    const tags = ['fashion', 'art', 'music']
     const title = faker.lorem.words(1);
     const description = faker.lorem.sentences(1)
     const articleInfo = faker.lorem.sentences(3)
     it('Create new article',function(){
-        cy.api({
-            method: "POST",
-            url: `${api_server}/articles`,
-            headers:{
-                Authorization: 'Token '+Cypress.env('token')
-            },
-            body:{
-                "article": {
-                    "title": title,
-                    "description": description,
-                    "body": articleInfo,
-                    "tagList": [
-                        'fashion', 'art', 'music'
-                    ]
-                }
-            }
-        }).then((response)=>{
+    articlePage.createNewArticle(title,description,articleInfo,tags)
+        .then((response)=>{
             expect(response.status).eq(201)
             expect(response.body.article.slug).include(title+'-2980')
-            
         })
-
     })
 
-    it('verify new post ',function(){
-        cy.api({
-            method:'GET',
-            url:`${api_server}/articles/${title}-2980`,
-            headers:{
-                Authorization: 'Token '+Cypress.env('token')
-            }
-        }).then((response)=>{
+    it('verify new post status and slug',function(){
+        articlePage.getArtucleByTitle(title)
+        .then((response)=>{
             expect(response.status).eq(200)
-           expect(response.body.article.slug).include(title+'-2980')
+            expect(response.body.article.slug).include(title+'-2980')
+        })
+    })
+    it('verify new post description and taglist lenght',function(){
+        articlePage.getArtucleByTitle(title)
+        .then((response)=>{
+            expect(response.body.article.description).eq(description)
+            expect(response.body.article.tagList).length(3)
         })
     })
 })
--- cypress/pages/articles.js
+++ cypress/pages/articles.js
@@ -1,5 +1,35 @@
+const api_server = Cypress.env('api_server')
+
 class Article{
 
+    createNewArticle(title,description,articleInfo,tags){
+        return cy.api({
+            method: "POST",
+            url: `${api_server}/articles`,
+            headers:{
+                Authorization: 'Token '+Cypress.env('token')
+            },
+            body:{
+                "article": {
+                    "title": title,
+                    "description": description,
+                    "body": articleInfo,
+                    "tagList": tags
+                }
+            }
+        })
+    }
+
+    getArtucleByTitle(title){
+        return cy.api({
+            method:'GET',
+            url:`${api_server}/articles/${title}-2980`,
+            headers:{
+                Authorization: 'Token '+Cypress.env('token')
+            }
+        })
+    }
     
 
 }
+export const articlePage = new Article()
